0c26545ca3dc9a1f60054d963e48e4390603caba
elsanaa1 <sanaa.jerbouh1@gmail.com>
2024-12-24 23:45:11 +0100

    second problem

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ for( let c of tab){
     { count++;}
 }
 console.log(tab)
+
+
+// Create a function that takes 
+// an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+// Solution:
+function ev_odd(num){
+  
+if(num%2==0){
+    return "Even"
+}else{
+    return "odd"
+}
+
+}
+
