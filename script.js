9a3591e35fe2effc2ab63646f59cd88441dd0712
elsanaa1 <sanaa.jerbouh1@gmail.com>
2024-12-25 19:27:43 +0100

    problems

--- script.js
+++ script.js
@@ -3,14 +3,17 @@
 // The input string will only consist of lower case letters and/or spaces.
 
 // Solution:
-let word=prompt("type a string:")
-let tab=word.split("")
-let count=0
-for( let c of tab){
-  if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u')
-    { count++;}
+function countv(word){
+    let tab=word.split("")
+    let count=0
+    for( let c of tab){
+      if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u')
+        { count++;}
+    }
+   return count;
+    
 }
-console.log(tab)
+
 
 
 // Create a function that takes 
@@ -25,4 +28,44 @@ if(num%2==0){
 }
 
 }
+
+// You are going to be given a non-empty string. Your job is to return the middle character(s) of the string.
+// If the string's length is odd, return the middle character.
+// If the string's length is even, return the middle 2 characters.
+// Solution:
+function middchar(word){
+    let len=word.length;
+    let div=len/2; 
+    if(Number.isInteger(div)){
+        let midd=word[Math.trunc(div)-1]
+        let midd2=word[Math.trunc(div)]
+        return midd+","+midd2
+    }else{
+            let midd=word[Math.trunc(div)]
+             return midd
+    }
+}
+
+
+// Opposite number:
+// Very simple, given a number (integer / decimal / both depending on the language),
+//  find its opposite (additive inverse).
+// Solution:
+
+function opposNum(num){
+    let sign=Math.sign(Number(num))
+    if(sign==0)
+    {
+         return num;
+    }else if(sign==1){
+        return "-"+num;
+    }
+    else{
+        return Math.abs(Number(num));
+    }
+}
+
+
+
+
 
