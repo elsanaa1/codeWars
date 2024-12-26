e711a078b4e51520294814d5dfdbebae3958b451
elsanaa1 <sanaa.jerbouh1@gmail.com>
2024-12-26 19:30:52 +0100

    Growth of a Population

--- script.js
+++ script.js
@@ -254,7 +254,21 @@ function highLow(w){
            let min2=Math.min(...arr)
            return min+min2
         }
+        // Growth of a Population
+        // p0, percent, aug (inhabitants coming or leaving each year), p (population to equal or surpass)
+        // the function nb_year should return n number of entire years needed to get a population greater or equal to p.
+        // aug is an integer, percent a positive or null floating number, p0 and p are positive integers (> 0)
+        // Examples:
+        // nb_year(1500, 5, 100, 5000) -> 15
 
-        
-
-
+        function nb_year(p0, percent, aug,p){
+            let nbr_p=p0;
+            percent=percent/100;
+            let nb_y=0
+            while(nbr_p<p){
+                nbr_p=nbr_p+ nbr_p *percent+ aug
+                nb_y++;
+            }
+            return nb_y;
+            
+        }
